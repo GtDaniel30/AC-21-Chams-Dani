5538df1855c6373f9b630f95390b0bd2ee30b169
Daniel <gildresdaniel1@gmail.com>
2025-12-12 12:14:35 +0100

    tasca

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("=== SISTEMA DE FABRICACIÓN DE NAVES ESTELARES ===");
+
+    }
+
+    static void CrearNave()
+    {
+        string[] nave = { "HALCONMILENARIO-", "CAZAESTELAR-", "SUPERDESTRUCTOR-", "YWING-", "XWING-", };
+        Random random = new Random();
+        int indice = random.Next(0, nave.Length);
+        int numero = random.Next(10, 100);
+        Console.WriteLine("Nave seleccionada: " + nave[indice] + numero);
+    }
+}
